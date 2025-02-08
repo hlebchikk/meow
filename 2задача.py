e7b1a39567993f756ce0be3cdd9513f58e25c5c1
hlebchikk <erry21onelove@gmail.com>
2025-02-09 00:16:28 +0200

    08.02.2025

---
+++ 2задача.py
@@ -0,0 +1,15 @@
+#Калькулятор
+a = int(input("Введите первое число:"))
+b = int(input("Введите второе число:"))
+c = input("Введите операцию, которую хотите совершить:")
+if c == "+":
+    res = (a + b)
+elif c == "-"
+    res = (a - b)
+elif c == "/":
+    res = (a / b)
+elif c == "*":
+    res = (a * b)
+else:None
+
+print(res)
