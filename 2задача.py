25e44227993c06f3ebe3dc83fc3a4e9701c0c589
hlebchikk <erry21onelove@gmail.com>
2025-02-14 17:40:57 +0200

    Update 2задача.py

--- 2задача.py
+++ 2задача.py
@@ -2,14 +2,19 @@
 a = int(input("Введите первое число:"))
 b = int(input("Введите второе число:"))
 c = input("Введите операцию, которую хотите совершить:")
+
 if c == "+":
-    res = (a + b)
-elif c == "-"
+    res = a + b
+elif c == "-":
     res = (a - b)
+elif (a == 0 or b == 0) and c == "/":
+    res = "You can't divide by 0!"
 elif c == "/":
     res = (a / b)
 elif c == "*":
-    res = (a * b)
-else:None
-
+    res = a * b
+elif c == "!=":
+    res = (a != b)
+else:
+    res = "Error"
 print(res)
