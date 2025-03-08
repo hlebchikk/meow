55d50ad1a60678ca1371f7d84c4a77cb44d6c9be
hlebchikk <erry21onelove@gmail.com>
2025-03-08 22:25:21 +0300

    Upgrade HW №1.2.py

--- HW №1.2.py
+++ HW №1.2.py
@@ -18,3 +18,29 @@ elif c == "!=":
 else:
     res = "Error"
 print(res)
+
+#Калькулятор Модифицированный
+while True:
+ a = int(input("Введите первое число:"))
+ b = int(input("Введите второе число:"))
+ c = input("Введите операцию, которую хотите совершить:")
+
+ if c == "+":
+    res = a + b
+ elif c == "-":
+    res = (a - b)
+ elif (a == 0 or b == 0) and c == "/":
+    res = "You can't divide by 0!"
+ elif c == "/":
+    res = (a / b)
+ elif c == "*":
+    res = a * b
+ elif c == "!=":
+    res = (a != b)
+ else:
+    res = "Error"
+ print(res)
+ print("Хотите продолжить работу калькулятора?")
+ answer = input("Yes or no?")
+ if answer not in ("yes"):
+     break
