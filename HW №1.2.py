13d60edb84165e3c5b0dc682662c8d03f86b88ae
hlebchikk <erry21onelove@gmail.com>
2025-03-12 15:43:47 +0200

    Update HW №1.2.py

--- HW №1.2.py
+++ HW №1.2.py
@@ -1,28 +1,28 @@
-#Калькулятор
-a = int(input("Введите первое число:"))
-b = int(input("Введите второе число:"))
-c = input("Введите операцию, которую хотите совершить:")
-
-if c == "+":
-    res = a + b
-elif c == "-":
-    res = (a - b)
-elif (a == 0 or b == 0) and c == "/":
-    res = "You can't divide by 0!"
-elif c == "/":
-    res = (a / b)
-elif c == "*":
-    res = a * b
-elif c == "!=":
-    res = (a != b)
-else:
-    res = "Error"
-print(res)
+# #Калькулятор
+# a = int(input("Введите первое число:"))
+# b = int(input("Введите второе число:"))
+# c = input("Введите операцию, которую хотите совершить:")
+#
+# if c == "+":
+#     res = a + b
+# elif c == "-":
+#     res = (a - b)
+# elif (a == 0 or b == 0) and c == "/":
+#     res = "You can't divide by 0!"
+# elif c == "/":
+#     res = (a / b)
+# elif c == "*":
+#     res = a * b
+# elif c == "!=":
+#     res = (a != b)
+# else:
+#     res = "Error"
+# print(res)
 
 #Калькулятор Модифицированный
 while True:
- a = int(input("Введите первое число:"))
- b = int(input("Введите второе число:"))
+ a = float(input("Введите первое число:"))
+ b = float(input("Введите второе число:"))
  c = input("Введите операцию, которую хотите совершить:")
 
  if c == "+":
@@ -42,5 +42,5 @@ while True:
  print(res)
  print("Хотите продолжить работу калькулятора?")
  answer = input("Yes or no?")
- if answer not in ("yes"):
+ if answer != "yes":
      break
