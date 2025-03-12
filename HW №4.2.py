6c7995bf78ed9492523c81f5d0dfe757abdc7038
hlebchikk <erry21onelove@gmail.com>
2025-03-12 15:21:19 +0200

    Update HW №4.2.py

--- HW №4.2.py
+++ HW №4.2.py
@@ -1,10 +1,21 @@
 from keyword import kwlist
 import string
 
+activator = True
+punctuation = string.punctuation.replace("_", "")
 stroka = input("Введите строку:")
 
-proverka = (stroka.count("_") <= 1, stroka not in kwlist , not stroka[0].isdigit(), not stroka == set(string.punctuation) - {"_"} , not any(i.isupper() for i in stroka))
-answer = all(proverka)
+if stroka.count("_") > 1:
+    activator = False
+elif stroka == kwlist:
+    activator = False
+elif stroka[0].isdigit():
+    activator = False
+elif stroka == punctuation:
+    activator = False
+elif stroka.isupper():
+    activator = False
+elif stroka == " ":
+    activator = False
 
-print(answer)
-
+print(activator)
