03300429b1479d42f7cce2952176dd638ea2bcd0
hlebchikk <erry21onelove@gmail.com>
2025-03-20 19:05:59 +0300

    Update HW №3.3.py

--- HW №3.3.py
+++ HW №3.3.py
@@ -1,23 +1,28 @@
-section1 = [i for i in range(8) if i > 0]
-section2 = [i for i in range(17) if i > 0]
-section3 = [i for i in range(10) if i > 0]
-print("Здравствуйте! Секций с местами на самолет всего три: 1 секция - 7 мест",
-      "2 секция - 16 мест",
-      "3 секция - 9 мест")
+all_section = {
+    1: 7,
+    2: 16,
+    3: 9
+}
+print("Здравствуйте! Секций с местами на самолет всего три:", all_section)
 while True: #Основной цикл
-    while True: #Цикл сейций
-        section = int(input("Выберите секцию, на которой хотите бронировать места (1-3): "))
+    while True: #Цикл секций
+        section = int(input("Выберите секцию, на которой хотите бронировать места (1-3) или нажмите 0 для выхода: "))
         if section == 0:
             print("Выход из системы.")
             exit()
-        if 1 <= section <= 3:
+        if section in all_section:
+            print(f"Вы выбрали секцию {section}, в ней {all_section[section]} мест.")
             break
-        print("Такой секции не существует, попробуйте еще раз")
+        else:
+            print("Введеное число некоректно, попробуйте снова.")
     while True: #Цикл мест
-        mesto = int(input("Отлично! Теперь выберите из той секции что вы выбрали: "))
+        mesto = int(input("Теперь, укажите нужное количество мест из той секции что вы выбрали или нажмите 0 для выхода: "))
         if mesto == 0:
             print("Выход из системы.")
             exit()
-        if 1 <= mesto <= 16:
+        if 1 <= mesto <= all_section[section]:
+            all_section[section] -= mesto
+            print(f"Бронь успешна! В секции {section} осталось {all_section[section]} мест.")
             break
-        print("Число мест больше существующих, попробуйте еще раз")
+        else:
+            print("Число мест больше либо меньше существующих, попробуйте еще раз.")
