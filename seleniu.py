6ab03f8ce0ead16d0a638f0c46d5acf22f616590
hlebchikk <erry21onelove@gmail.com>
2025-08-03 23:11:07 +0300

    Add files via upload

--- seleniu.py
+++ seleniu.py
@@ -1,76 +1,112 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from string import punctuation
 
-# Запускаем браузер и открываем сайт
-driver = webdriver.Chrome()
-driver.get("https://www.work.ua/jobs-odesa-it")
 
-vacancy_container = driver.find_element(By.ID, "pjax-jobs-list")
 
-vacancy_cards = vacancy_container.find_elements(By.CLASS_NAME, "card")
+class WorkParser:
+    def __init__(self, url):
+        self.url = url
+        self.driver = webdriver.Chrome()
+        self.vacancy_cards = []
+        self.raw_salaries = []
+        self.middle_salary = 0
 
-cyrillic_letters = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"
-letters = cyrillic_letters + cyrillic_letters.upper()
+    def scroll_through_pages(self):
+        while True:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "pjax-jobs-list")))
 
-#Удаляем из пунктуации дефис
-punct_to_remove = punctuation.replace("-", " ")
+            vacancy_container = self.driver.find_element(By.ID, "pjax-jobs-list")
+            cards = vacancy_container.find_elements(By.CLASS_NAME, "card")
+            self.vacancy_cards.extend(cards)
 
-#Сюда будут складываться все зарплаты
-raw_salaries = []
+            try:
+                next_page = self.driver.find_element(By.LINK_TEXT, "Наступна")
+                if "disabled" in next_page.get_attribute("class"):
+                    break
+                next_page.click()
 
-for i in vacancy_cards:
-    strong_600 = i.find_elements(By.CLASS_NAME, "strong-600")
-    for j in strong_600:
-        if "грн" not in j.text:
-            continue
-        raw_salaries.append(j.text)
+                WebDriverWait(self.driver, 10).until( EC.staleness_of(cards[0]))
+            except:
+                break
 
-salaries = []
-# Обрабатываем каждый элемент с "зарплатой"
-for i in raw_salaries:
-    text = i.strip()
+    def collect_all_vacancy_cards(self):
+        self.driver.get(self.url)
+        self.scroll_through_pages()
+        vacancy_container = self.driver.find_element(By.ID, "pjax-jobs-list")
 
-    # Удаляем неразрывные пробелы и обычные пробелы
-    text = text.replace('\u202f', '').replace('\u2009', '').replace(' ', '')
+    def remove_symbol(self, text):
+        cyrillic_letters = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"
+        letters = cyrillic_letters + cyrillic_letters.upper()
+        punct_to_remove = punctuation.replace("-", " ")
+        symbols = ['\u202f', '\u2009', ' ']
 
-    # Пропускаем, если вообще нет цифр
-    if not any(j.isdigit() for j in text):
-        continue
+        for i in symbols:
+            text = text.replace(i, '')
+        for l in text:
+            if l in letters or l in punct_to_remove:
+                text = text.replace(l, '')
+        return text
 
-    #Меняем толстый дефис на обычный, так как зарплаты обозначены им
-    text = text.replace("–", "-")
+    def extract_salaries(self):
+        for card in self.vacancy_cards:
+            try:
+                strong_600 = card.find_elements(By.CLASS_NAME, "strong-600")
+                for j in strong_600:
+                    if "грн" not in j.text:
+                        continue
+                    self.raw_salaries.append(j.text)
+            except Exception as ex:
+                print("Ошибка доступа к элементу:", ex)
 
-    # Удаляем кириллицу и знаки препинания
-    for l in text:
-        if l in letters or l in punct_to_remove:
-            text = text.replace(l, "")
+    def calculate_average_salary(self):
+        salaries = []
 
-    # Разбиваем диапазон зарплат по дефису
-    digits_text = text.split("-")
+        for i in self.raw_salaries:
+            text = i.strip()
+            if not any(c.isdigit() for c in text):
+                continue
 
-    #Превращаем все в целые числа, если вылазит ошибка, говорим об этом и пропускаем число
-    try:
-        digits = list(map(int, digits_text))
-    except ValueError as ex:
-        print("Ошибка преобразования:", ex)
-        continue
+            text = text.replace("–", "-")
+            text = self.remove_symbol(text)
+            digits_text = text.split("-")
 
-    #Тут если 2 числа делаем среднее, или просто добавляем 1 число
-    if digits:
-        if len(digits) > 1:
-            salary = sum(digits) / len(digits)
+            try:
+                digits = list(map(int, digits_text))
+            except ValueError as ex:
+                print("Ошибка преобразования:", ex)
+                continue
+
+            if digits:
+                if len(digits) > 1:
+                    salary = sum(digits) / len(digits)
+                else:
+                    salary = digits[0]
+                salaries.append(salary)
+
+        print(salaries)
+
+        if salaries:
+            self.middle_salary = round(sum(salaries) / len(salaries))
         else:
-            salary = digits[0]
+            self.middle_salary = 0
 
-    salaries.append(salary)
+    def play(self):
+        self.collect_all_vacancy_cards()
+        self.scroll_through_pages()
+        self.extract_salaries()
+        self.calculate_average_salary()
+        self.close()
+        print(f"Средняя зарплата: {self.middle_salary}")
+
+    def close(self):
+        if self.driver:
+            self.driver.quit()
 
 
-print(salaries)
-
-#Делим и округляем
-middle_salary = round(sum(salaries)/len(salaries))
-
-print(f"Средняя зарплата: {middle_salary}")
-
-driver.quit()
+if __name__ == "__main__":
+    url = "https://www.work.ua/jobs-odesa-it"
+    parser = WorkParser(url)
+    parser.play()
